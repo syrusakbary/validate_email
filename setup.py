ce2ca7d1fa504b0d33a9cade8eb4c8fcffac8c02
Syrus Akbary <me@syrusakbary.com>
2012-04-22 22:13:19 +0200

    Fixed module bug

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(name='validate_email',
       version = '1.0',
       download_url = 'git@github.com:syrusakbary/validate_email.git',
-      py_modules = 'validate_email',
+      py_modules = ('validate_email',),
       author = 'Syrus Akbary',
       author_email = 'me@syrusakbary.com',
       description = 'Validate_email verify if an email address is valid and really exists.',
