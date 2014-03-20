b7c58615881dbbf8dec64ae67748bdb551d6f8e6
Syrus Akbary <me@syrusakbary.com>
2014-03-20 12:41:39 +0100

    Updated version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='validate_email',
-      version = '1.1',
+      version = '1.2',
       download_url = 'git@github.com:syrusakbary/validate_email.git',
       py_modules = ('validate_email',),
       author = 'Syrus Akbary',
