9f9fc94875f9f081cb3c24389f2f3e74b4a27d01
Ben Baert <ben_b@gmx.com>
2018-05-31 13:13:28 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='validate_email',
-      version = '1.3',
-      download_url = 'git@github.com:syrusakbary/validate_email.git',
-      py_modules = ('validate_email',),
-      author = 'Syrus Akbary',
-      author_email = 'me@syrusakbary.com',
-      description = 'validate_email verifies if an email address is valid and really exists.',
-      long_description=open('README.rst').read(),
-      keywords = 'email validation verification mx verify',
-      url = 'http://github.com/syrusakbary/validate_email',
-      license = 'LGPL',
-    )
+setup(
+    name="pyemailval",
+    version="1.0",
+    download_url="git@github.com:ben-baert/pyemailval.git",
+    py_modules=("pyemailval",),
+    author="Ben Baert",
+    author_email="ben_b@gmx.com",
+    description="pyemailval verifies if an email address really exists.",
+    long_description=open("README.rst").read(),
+    keywords="email validation verification mx verify",
+    url="http://github.com/ben-baert/pyemailval",
+    license="LGPL",
+)
