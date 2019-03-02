3343039995051bf9b49e42d7395d600856749cef
László Károlyi <laszlo@karolyi.hu>
2019-03-02 02:20:16 +0100

    Fixing

--- validate_email/validate_email.py
+++ validate_email/validate_email.py
@@ -6,7 +6,8 @@ def validate_email(
         email_address: str, check_regex: bool = True, check_mx: bool = True,
         smtp_timeout: int = 10, use_blacklist: bool = True) -> bool:
 
-    if check_regex and not regex_check(email_address):
+    if check_regex and not regex_check(
+            value=email_address, use_blacklist=use_blacklist):
         return False
 
     if check_mx and not mx_check(email_address, smtp_timeout=smtp_timeout):
