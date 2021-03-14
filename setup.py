92194c93d538600fe5e1c2f46b66a223e8423a40
László Károlyi <laszlo@karolyi.hu>
2021-03-14 14:06:46 +0100

    Bump idna dependency

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     name='py3-validate-email',
     version='0.2.16',
     packages=find_packages(exclude=['tests']),
-    install_requires=['dnspython~=2.0', 'idna~=2.10', 'filelock~=3.0'],
+    install_requires=['dnspython~=2.0', 'idna~=3.0', 'filelock~=3.0'],
     author='László Károlyi',
     author_email='laszlo@karolyi.hu',
     description=(
